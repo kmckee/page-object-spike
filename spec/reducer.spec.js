77c3c1c6ccd9392fbf5e015e24fad32c22af0ac9
Kyle McKee <mckee.kyle@gmail.com>
2016-02-14 22:07:06 -0500

    Such functional.  Much reduce.

--- spec/reducer.spec.js
+++ spec/reducer.spec.js
@@ -2,7 +2,7 @@ const reduce = require('../lib/reducer').reducePromises;
 
 describe('reducer', () => {
     "use strict";
-    it('can reduce a single, serial promise', (done) => {
+    it('can reduce a single serial promise', (done) => {
         let promises = [() => { return Promise.resolve('foo'); }]
         let result = reduce(promises);
         result.then((value) => {
@@ -10,7 +10,7 @@ describe('reducer', () => {
             done();
         });
     });
-    it('can reduce two, serial promises', (done) => {
+    it('can reduce two serial promises', (done) => {
         let promises = [
             () => { return Promise.resolve('foo'); },
             () => { return Promise.resolve('bar'); }
