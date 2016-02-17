a4978b5fab3e7da86ad1ee1a45a05473fbcea2bc
Kyle McKee <mckee.kyle@gmail.com>
2016-02-17 12:26:53 -0500

    Refactoring.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 module.exports            = {};
+
+// Navigation
 module.exports.on         = require('./dist/builder').on;
 module.exports.visit      = require('./dist/builder').visit;
+
+// Browser
 module.exports.setInstance = require('./dist/browser').setInstance;
 module.exports.getInstance = require('./dist/browser').getInstance;
+
+// Elements
 module.exports.Button     = require('./dist/elements/Button');
 module.exports.Span       = require('./dist/elements/Span');
