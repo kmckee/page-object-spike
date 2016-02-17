73538fe57967dd3a103cbe282fe8e17704287255
Kyle McKee <mckee.kyle@gmail.com>
2016-02-17 12:25:31 -0500

    Moved elements into their own folder.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ module.exports.on         = require('./dist/builder').on;
 module.exports.visit      = require('./dist/builder').visit;
 module.exports.setInstance = require('./dist/browser').setInstance;
 module.exports.getInstance = require('./dist/browser').getInstance;
-module.exports.Button     = require('./dist/Button');
-module.exports.Span       = require('./dist/Span');
+module.exports.Button     = require('./dist/elements/Button');
+module.exports.Span       = require('./dist/elements/Span');
